414e61f05786e2d5b36df7c66496ae3a2b10dd26
Lalit <86079474+lalit0801@users.noreply.github.com>
2024-04-19 18:24:52 +0530

    Update views.py

--- VoiceChatApp/views.py
+++ VoiceChatApp/views.py
@@ -1,7 +1,8 @@
 from rest_framework import generics
 from django.shortcuts import render
 from .RetailLLM import *
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest, QueryDict
+from rest_framework.request import Request
 from .Google import Create_Service
 from datetime import datetime,timedelta
 from rest_framework.response import Response
@@ -9,8 +10,9 @@ from rest_framework.views import APIView
 from rest_framework import status
 import os
 from rest_framework.views import APIView
-from rest_framework.response import Response
 from openai import OpenAI
+import json
+import requests
 
 
 CLIENT_SECRET_FILE = os.path.abspath('new_one.json')
@@ -138,8 +140,8 @@ class ChatBotView(generics.CreateAPIView):
                     "content": f"""
                     !IMPORTANT Please follow these steps one by one:
                     1. Your purpose is to analyze {previous_questions}. If you find anything only related to "what services you provide" , respond with "BOOK AN APPOINTMENT. Type: "Confirm My Appointment" TO CONFIRM YOUR APPOINTMENT AND SEE FOR AVAILABLE SLOTS".Otherwise be crypto assistant.
-                    2. After first step , analyze {previous_questions}. If you find the exact words "Confirm My Appointment". Respond with Message "Available Slots will be shown. Enter Details in the same format: name: "your_name", start_time:"12:45 PM"".Otherwise be crypto assistant.
-                    3. After second step, You  purpose is to  analyze {previous_questions}. If you find user input in or related format "name: "your_name", start_time:"12:45 PM"" then respond with Message :"Success" and convert Data in json format having all the information. Otherwise be crypto assistant.
+                    2. After first step , analyze {previous_questions}. If you find the exact words "Confirm My Appointment". Respond with Message "Available Slots will be shown. Enter Details in the same format: title: "your_event_title", length of event:"35"".Otherwise be crypto assistant.
+                    3. After second step, You  purpose is to  analyze {previous_questions}. If you find user input in or related format "title: "your_event_title", length of event:"35"" then strictly respond with Message :"Success" along with convert Data in json format having all the information. Otherwise be crypto assistant.
                     4.
                     """
                 },
@@ -147,10 +149,7 @@ class ChatBotView(generics.CreateAPIView):
             for question in previous_questions :
                 chat_history.append({'role' : 'user', 'content' : question})
 
-            # setup for the system
-            # setup = request.data['setup']
-
-            # if prompt.lower()= "what services you provide"
+            
             # open ai chat response
             response = client.chat.completions.create(
                         model="gpt-4-turbo",
@@ -160,8 +159,10 @@ class ChatBotView(generics.CreateAPIView):
                         )
 
             content = response.choices[0].message.content
+            print(type(content))
             print(content)
             print(request.path,"path here")
+            
             # Calling create_event API based on content recieved
             if "Success" in content:
                 # Extracting JSON content from the response
@@ -174,14 +175,41 @@ class ChatBotView(generics.CreateAPIView):
                         json_data = json.loads(json_content)
                         print("json converted")
                         print(json_data)
+                        print(type(json_data))
 
                         # Calling create_event API based on content received
                         print("******")
-                        # setting request data
+                        
+                        # Establish a connection to the server
+                        reqUrl= "https://api.cal.com/v1/event-types?apiKey=cal_live_0aa50476d76c04be2e1d9b16e961081b"
+                        # https://api.cal.com/v1/event-types?apiKey=cal_test_xxxxxx
+                        print("cal.com api hit")
+                        payload = {
+                            "length": int(json_data.get("length of event")),
+                            "title": json_data.get("title"),
+                            "metadata": {},
+                            "slug": json_data.get("title").replace(" ", "-").lower()  # Generating slug from the title
+                        }
 
-                        create_event = CreateEvent()
-                        response = create_event.post(request=request)
-                        print(response.data,"response_data")
+                        # Convert the payload dictionary to JSON string
+                        payload_json = json.dumps(payload)
+                    
+                        # payload = json.dumps(json_data)
+                        print(payload,"data in payload")
+                        # print(payload,"______")
+
+                        response= requests.request("POST", reqUrl, data=payload_json,  headers=request.headers)
+                        print("response executed")
+                        if response.status_code == 200:
+                    # Return success response
+                            print("success")
+                        else:
+                            # Return error response
+                            print("failed")
+                
+                        # if response.text == "booked successfully":
+                        #   content= response.text
+                        # else: content= "Failed to schedule event"
 
             
             
